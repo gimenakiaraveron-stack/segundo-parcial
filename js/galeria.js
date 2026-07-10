be289340a07675ff2b8f704b8a786c1990da3e26
gimenakiaraveron-stack <gimenakiaraveron@gmail.com>
2026-07-10 18:01:03 -0300

    Cree array de obras en galeria.js:
    Cree el array de objetos con la información de las obras que se utilizarán en galería. Me queda pendiente agregar las imágenes y completar la funcionalidad.

---
+++ js/galeria.js
@@ -0,0 +1,31 @@
+//Galería de obras de Rafael Lozano-Hemmer
+const obras= [
+{
+    nombre: "Pulse Room",
+    año: 2006,
+    imagen:"img/lozano-hemmer-1.jpg" 
+
+},
+{
+    nombre: "Zoom Pavillon",
+    año:2015,
+    imagen:""
+
+},
+{
+    nombre:"Border tuner",
+    año:2019,
+    imgen:""
+},
+{
+    nombre:"33 Questions per minute",
+    año:2000,
+    imagen:""
+},
+{
+    nombre:"Sandbox",
+    año:2010,
+    imagen:""
+}
+];
+//termina el array
